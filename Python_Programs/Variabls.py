2a971f9f6c138ef10cdce31a3231ddef9cd3ec74
Rajkumargit99 <rajkumar1193.k@gmail.com>
2026-02-09 22:51:58 +0530

    python Basic programs 1 to 15

--- Python_Programs/Variabls.py
+++ Python_Programs/Variabls.py
@@ -1,3 +1,100 @@
-a=10
-b=20
-print(a+b)
+
+# 1). Python Program to add two integer values.
+
+a = 10
+b = 20
+print("Addition integer value of a+b:", a+b)
+c = a+b
+print("Addition integer value of c:", c)
+
+x,y,z = 5,10,20
+print("Addition integer value of x+y+z:", x+y+z)
+
+# 2). Python Program to subtract two integer values.
+
+a1 = 50
+b1 = 20
+print("Subtraction integer value of a1-b1:", a1-b1)
+c1 = a1-b1
+print("Subtraction integer value of c1:", c1)
+
+# 3). Python program to multiply two numbers.
+
+a2 = 4
+b2 = 2
+print("Multiply value of a2*b2:", a2*b2)
+c2 = a2*b2
+print("Multiply value of c2:", c2)
+
+#4). Python program to repeat a given string 5 times.
+'''Input :
+str1 = “SQATools”
+Output :
+“SQAToolsSQAToolsSQAToolsSQAToolsSQATools” 
+'''
+str1 = "SQATools"
+print("Result:", str1*5)
+
+# 5). Python program to get the Average of given numbers.
+'''Formula: sum of all the number/ total number
+'''
+a = 40
+b = 50
+c = 30
+print((a+b+c)/3)
+
+
+'''# 6). Python program to get the median of given numbers.
+Note: all the numbers should be arranged in ascending order
+Formula : (n+1)/2
+n = Number of values
+Input : [45, 60, 61, 66, 70, 77, 80]
+Output:  66
+
+Solution
+#7). Python program to print the square and cube of a given number.
+Input :
+num1 = 9
+Output :
+Square = 81
+Cube =   729
+
+Solution
+#8). Python program to interchange values between variables.
+Input :
+a = 10
+b = 20
+Output :
+a = 20
+b = 10
+
+Solution
+#9). Python program to solve this Pythagorous theorem.
+Theorem : (a2 + b2 = c2)
+
+Solution
+#10). Python program to solve the given math formula.
+Formula : (a + b)2 = a^2 + b^2 + 2ab
+
+Solution
+#11). Python program to solve the given math formula.
+Formula : (a – b)2 = a^2 + b^2 – 2ab
+
+Solution
+#12). Python program to solve the given math formula.
+Formula : a2 – b2 = (a-b)(a+b)
+
+Solution
+#13). Python program to solve the given math formula.
+Formula : (a + b)3 = a3 + 3ab(a+b) + b3 
+
+Solution
+#14). Python program to solve the given math formula.
+Formula : (a – b)3 = a3 – 3a2b + 3ab2 – b3
+
+Solution
+#15). Python program to calculate the area of the square.
+Formula : area = a*a
+
+Solution
+'''
